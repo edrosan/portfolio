6b82de396832aa561c8f8803ffc489bebf5c8055
Eduardo <edo.rod.san@gmail.com>
2023-05-29 00:02:48 -0600

    edit

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,7 +2,7 @@ import user from '../src/assets/img/user.png';
 
 export function Profile() {
   return (
-    <div className="w-full flex justify-between">
+    <div className="w-full flex flex-col justify-between md:flex-row">
       <div className="w-auto h-full flex flex-col ">
         <h1 className="text-2xl text-zinc-600 font-bold font-sans">EDUARDO RODRIGUEZ SANCHEZ</h1>
         <p className="text-xl text-zinc-600">Programador Web</p>
