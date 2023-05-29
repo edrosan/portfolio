f0af3f9d313b7f1ef6412643603a365851630ab7
Eduardo <edo.rod.san@gmail.com>
2023-05-29 01:25:14 -0600

    edit

--- src/Proyecto.jsx
+++ src/Proyecto.jsx
@@ -1,12 +1,12 @@
-export function Proyecto({ img, tecnologias }) {
+export function Proyecto({ imagenProyecto, linkTo, tecnologias }) {
   const tecUsadas = [...tecnologias];
 
   return (
     <a
-      href={img}
+      href={linkTo}
       className="w-full h-[200px] bg-white border border-gray-200 rounded-lg relative"
     >
-      <img className="object-cover w-full h-full rounded-lg" src={img} />
+      <img className="object-cover w-full h-full rounded-lg" src={imagenProyecto} />
 
       <div className="absolute bottom-0 h-10 flex gap-2 justify-center items-center bg-indigo-500 text-zinc-50 w-full rounded-b-lg">
         {tecUsadas.map((tecnologia, index) => {
