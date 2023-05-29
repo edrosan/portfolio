2349d21d6d0316c6b49c316ef453cd51f8ec02bf
Eduardo <edo.rod.san@gmail.com>
2023-05-29 15:05:10 -0600

    edit

--- src/IconLightMode.jsx
+++ src/IconLightMode.jsx
@@ -2,7 +2,7 @@ export function IconLightMode() {
 
   return(
     <svg
-      className="fill-[#e5e1e6] w-auto h-full"
+      className="fill-[#e5e1e6] w-auto h-full py-1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
     >
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,12 +1,16 @@
-import user from '../src/assets/img/user.png';
+import user from "../src/assets/img/user.png";
 
 export function Profile() {
   return (
-    <div className="w-full mb-16 bg-[#fffbff] flex flex-col items-center md:flex-row justify-between dark:bg-[#1c1b1f]">
+    <div className="w-full h-100% mb-24 bg-[#fffbff] flex flex-col items-center md:flex-row justify-between dark:bg-[#1c1b1f]">
       <div className="w-full h-full mb-4 flex flex-col ">
-        <h1 className="text-4xl mb-4 text-[#1c1b1f] font-bold font-sans dark:text-[#e5e1e6]">EDUARDO RODRIGUEZ SANCHEZ</h1>
-        <p className="text-xl text-[#1c1b1f] dark:text-[#e5e1e6]">
-        Un desarrollador web enfocado en frontend que construye el frontend de sitios web y aplicaciones web que conducen al éxito del producto en general.
+        <h1 className="text-4xl mb-8 text-[#1c1b1f] font-bold font-sans dark:text-[#e5e1e6]">
+          EDUARDO RODRIGUEZ SANCHEZ
+        </h1>
+        <p className="mb-10 text-xl text-[#1c1b1f] dark:text-[#e5e1e6]">
+          Un desarrollador web enfocado en frontend que construye el frontend de
+          sitios web y aplicaciones web que conducen al éxito del producto en
+          general.
         </p>
       </div>
       <img
