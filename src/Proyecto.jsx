a4442b27747f42d27a5ef11dfc51e9387604b116
Eduardo <edo.rod.san@gmail.com>
2023-05-29 00:39:55 -0600

    edit

--- src/Proyecto.jsx
+++ src/Proyecto.jsx
@@ -1,19 +1,22 @@
-import iconHtml from "./assets/img/icon-html.svg";
-import iconJavascript from "./assets/img/icon-javascript.svg";
+export function Proyecto({ img, tecnologias }) {
+  const tecUsadas = [...tecnologias];
 
-export function Proyecto({ img }) {
   return (
-    <div className="w-full h-[200px] bg-white border border-gray-200 rounded-lg relative">
+    <a
+      href={img}
+      className="w-full h-[200px] bg-white border border-gray-200 rounded-lg relative"
+    >
       <img className="object-cover w-full h-full rounded-lg" src={img} />
 
-      <div className="absolute bottom-0 h-5 flex  bg-indigo-500 text-zinc-50 w-full rounded-b-lg">
-        <span>
-          JavaScript
-        </span>
-        <span>
-          HTML
-        </span>
+      <div className="absolute bottom-0 h-10 flex gap-2 justify-center items-center bg-indigo-500 text-zinc-50 w-full rounded-b-lg">
+        {tecUsadas.map((tecnologia, index) => {
+          // Todo crear key unica
+          console.log(tecnologia+index)
+          return <p key={tecnologia+index} className="align-middle">
+            {tecnologia}
+            </p>
+        })}
       </div>
-    </div>
+    </a>
   );
 }
--- src/Proyectos.jsx
+++ src/Proyectos.jsx
@@ -6,9 +6,9 @@ export function Proyectos() {
       <h1 className="text-xl text-zinc-600 font-semibold">PROYECTOS</h1>
       <h2>Aplicaciones Web</h2>
       <div className="flex flex-col gap-2">
-        <Proyecto img="https://images.unsplash.com/photo-1684785805426-d3cc80537a51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"></Proyecto>
-        <Proyecto img="https://images.unsplash.com/photo-1684524961062-1afe34e5c347?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"></Proyecto>
-        <Proyecto img="https://images.unsplash.com/photo-1683470432347-7b40520fb583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"></Proyecto>
+        <Proyecto tecnologias={["HTML", "CSS", "JavaScript"]} img="https://images.unsplash.com/photo-1684785805426-d3cc80537a51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"></Proyecto>
+        <Proyecto tecnologias={["HTML", "CSS", "JavaScript"]} img="https://images.unsplash.com/photo-1684524961062-1afe34e5c347?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"></Proyecto>
+        <Proyecto tecnologias={["HTML", "CSS", "JavaScript"]} img="https://images.unsplash.com/photo-1683470432347-7b40520fb583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"></Proyecto>
       </div>
     </div>
   );
