96e610270dac7e81d5a896aed0edef777dbc2b74
Eduardo <edo.rod.san@gmail.com>
2023-05-29 15:27:08 -0600

    edit

---
+++ src/Skill.jsx
@@ -0,0 +1,7 @@
+export function Skill({children}) {
+  return (
+    <span className="bg-[#e4e1ec] text-[#47464f] text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-[#47464f] dark:text-[#c8c5d0]">
+      {children}
+    </span>
+  );
+}
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,3 +1,5 @@
+import { Skill } from "./Skill";
+
 export function Skills() {
   return (
     <div className="mb-16">
@@ -6,21 +8,13 @@ export function Skills() {
       SKILLS
       </h1>
 
-      <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-        HTML
-      </span>
+    <div>
+      <Skill>HTML</Skill>
+      <Skill>CSS</Skill>
+      <Skill>JavaScript</Skill>
+      
+    </div>
 
-      <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-        CSS
-      </span>
-
-      <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-        JavaScript
-      </span>
-
-      <span className="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
-        PHP
-      </span>
     </div>
   );
 }
