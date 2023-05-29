854498a0e3759dc71d311c207f60b1f5efd7ca9d
Eduardo <edo.rod.san@gmail.com>
2023-05-29 00:13:40 -0600

    edit

--- src/Proyecto.jsx
+++ src/Proyecto.jsx
@@ -3,7 +3,7 @@ import iconJavascript from "./assets/img/icon-javascript.svg";
 
 export function Proyecto({ img }) {
   return (
-    <div className="w-[300px] h-[200px] bg-white border border-gray-200 rounded-lg relative">
+    <div className="w-full h-[200px] bg-white border border-gray-200 rounded-lg relative">
       <img className="object-cover w-full h-full rounded-lg" src={img} />
 
       <div className="absolute bottom-0 h-5 flex  bg-indigo-500 text-zinc-50 w-full rounded-b-lg">
